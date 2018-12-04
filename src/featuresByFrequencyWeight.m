31c0a56ac52c66c2e8ef7c3872f9d43febd1660f
João Gabriel Dalavalle <joaogabriel1997@gmail.com>
2018-12-04 15:06:38 -0300

    Initial commit

---
+++ src/featuresByFrequencyWeight.m
@@ -0,0 +1,28 @@
+function Vetor = featuresByFrequencyWeight(dirWAV, timeStart, timeEnd)
+
+ %Lendo o arquivo .wav
+            [X,Fs]=audioread(dirWAV);
+
+ % para cada arquivo wav, gerar espectrograma
+            WINDOW=512;
+            NOVERLAP=WINDOW/2;
+            NFFT=WINDOW;
+            [S,~,~,~] =spectrogram(X,WINDOW,NOVERLAP,NFFT,Fs,'yaxis');
+            
+ %Salvando a matrix numa varíavel
+            Spectrograma = abs(S);
+            
+ %Para cada espectrograma gerar o vetor de características  
+     
+     trecho = Spectrograma(1:end, timeStart:timeEnd); %definindo a área de interesse
+     numberofRows = size(trecho,1); %Pegando o número de linhas do trecho
+	 pesos = (1 : numberofRows); %Criando o vetor de pesos sequencial 
+     Vetor = sum(bsxfun(@times, pesos', trecho)); %Multiplicando as linhas pelos pesos e somando
+     
+ %Salvando o vetor em um arquivo .csv
+ %      dirVET= strcat(dirVET,'.csv');
+ %     xlswrite(dirVET, Vetor);
+       
+       return 
+
+end
---
+++ test/gerarVetores.m
@@ -0,0 +1,78 @@
+allFeatures = [];
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/6_RN140.wav', 1000, 1300);
+allFeatures = [allFeatures ; features, 6];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/6_RN1264.wav', 4000, 4300);
+allFeatures = [allFeatures ; features, 6];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/6_RN1933.wav', 1, 301);
+allFeatures = [allFeatures ; features,6];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/6_RN5469.wav', 500, 800);
+allFeatures = [allFeatures ; features,6];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/6_RN7179.wav', 100, 400);
+allFeatures = [allFeatures ; features,6];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/10_RN6179.wav', 600, 900);
+allFeatures = [allFeatures ; features, 10];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/10_RN6622.wav', 2700, 3000);
+allFeatures = [allFeatures ; features, 10];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/10_RN6632.wav', 1300, 1600);
+allFeatures = [allFeatures ; features, 10];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/10_RN9137.wav', 7700,8000);
+allFeatures = [allFeatures ; features, 10];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/10_RN11209.wav', 1, 301);
+allFeatures = [allFeatures ; features, 10];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/11_RN998.wav', 1, 301);
+allFeatures = [allFeatures ; features, 11];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/11_RN4899.wav', 1, 301);
+allFeatures = [allFeatures ; features, 11];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/11_RN7120.wav', 300, 600);
+allFeatures = [allFeatures ; features, 11];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/11_RN7631.wav', 3300, 3600);
+allFeatures = [allFeatures ; features, 11];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/11_RN7836.wav', 400, 700);
+allFeatures = [allFeatures ; features, 11];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/21_RN3068.wav', 1000, 1300);
+allFeatures = [allFeatures ; features, 21];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/21_RN3385.wav', 100, 400);
+allFeatures = [allFeatures ; features, 21];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/21_RN7654.wav', 1000, 1300);
+allFeatures = [allFeatures ; features, 21];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/21_RN7699.wav', 600, 900);
+allFeatures = [allFeatures ; features, 21];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/21_RN8954.wav', 100, 400);
+allFeatures = [allFeatures ; features, 21];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/28_RN8177.wav', 5800, 6100);
+allFeatures = [allFeatures ; features, 28];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/28_RN9845.wav', 700, 1000);
+allFeatures = [allFeatures ; features, 28];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/28_RN11086.wav', 1200, 1500);
+allFeatures = [allFeatures ; features, 28];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/28_RN11363.wav', 800, 1100);
+allFeatures = [allFeatures ; features, 28];
+
+features = featuresByFrequencyWeight('/home/digao/Documentos/JoaoWavs/28_RN11682.wav', 600, 900);
+allFeatures = [allFeatures ; features, 28];
+
+dirVET= strcat('/home/digao/Documentos/JoaoVetores','/Vetores.csv');
+xlswrite(dirVET, allFeatures);
